f46a5225e82d986a878bd48afa71191972d203d1
ingacerkovska <92845733+ingacerkovska@users.noreply.github.com>
2022-03-18 18:04:06 +0200

    +

---
+++ script.js
@@ -0,0 +1,45 @@
+const API = "https://chat.ingacerkovska.repl.co"
+let zina = document.querySelector('#manaZina');
+let zinas = document.querySelector('.chataZinas');
+let vards = document.querySelector('#vards');
+
+function sutitZinu(){
+   // console.log('sutitZinu() darbojas');
+    
+    zinas.innerHTML= zinas.innerHTML+'<br/>'+zina.value;
+
+    fetch(API+'/sutit/'+vards.value+'/'+zina.value)
+}
+
+//async function ieladetChataZinas(){
+ //   let datiNoServera = await fetch(API +'/lasit');
+  //  let dati = await datiNoServera.text();
+  //  zinas.innerHTML = dati;
+//}
+
+//setInterval(ieladetChataZinas, 1000)
+
+async function ieladetChataZinasJson(){
+    let datiNoServera = await fetch(API +'/lasit');
+    let dati = await datiNoServera.json();
+    
+    //console.log(await dati[0]['zina'])
+    zinas.innerHTM='';
+
+
+    let i=0;
+    while (i<await dati.length){
+       // console.log(i); 
+       let laiks='[<i>' +'???   '+ '</i>]';
+      if ("laiks" in dati[i]){
+        laiks='[<i>'+dati[i]['laiks']+ '</i>]';
+      }
+
+
+        zinas.innerHTML=zinas.innerHTML+laiks+dati[i]['vards'] +':'+dati[i]['zina']+'<br/>';
+
+        i++;
+    }
+  zinas.scrollTop=zinas.scrollHeight;
+}//šeit beidzas funkcija ieladetChataZinasJson()  
+setInterval(ieladetChataZinasJson, 1000)
